fc19878ba803656c2448ed844a286387a7e5385b
Connor Kuehl <cipkuehl@gmail.com>
2020-09-25 17:24:27 -0500

    WIP: Turn on paging

--- kernel/src/asm.rs
+++ kernel/src/asm.rs
@@ -1,2 +1,11 @@
+/// Protected Mode
+pub const CR0_PE: u32 = 0x00000001;
+
+/// Write Protect
+pub const CR0_WP: u32 = 1 << 5;
+
+/// Paging
+pub const CR0_PG: u32 = 1 << 31;
+
 /// Page Size Extension (Use 4MiB-sized pages)
 pub const CR4_PSE: u32 = 1 << 4;
